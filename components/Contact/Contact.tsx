f216be80e8c0125719ed531e6e7b8dbd5ff92b23
David Guo <davidguo123456@gmail.com>
2024-12-01 22:34:37 -0500

    eslint

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import { IconBrandGithubFilled, IconBrandInstagram, IconBrandLinkedin, IconBrandTwitter, IconBrandYoutube, IconFileCv } from '@tabler/icons-react';
+import { IconBrandGithubFilled, IconBrandLinkedin, IconFileCv } from '@tabler/icons-react';
 import { ActionIcon, SimpleGrid, Text, Title, Container, Group} from '@mantine/core';
 import { ContactIconsList } from './ContactIcons';
 import classes from './Contact.module.css';
@@ -11,24 +11,23 @@ export function ContactUs() {
         <SimpleGrid cols={{ base: 1, sm: 2 }} spacing={50}>
           <div>
             <Title className={classes.title}>Contact Me</Title>
-            <Text className={classes.description} mt="sm" mb={30}></Text>
-            <ContactIconsList />
+            <ContactIconsList/>
           </div>
         </SimpleGrid>
         <Group gap="xs" mt={10}>
           <ActionIcon size="lg" color="white" variant="subtle">
             <a href={LINKS.linkedin} target="_blank" rel="noreferrer" style={{ color: 'white' }}>
-              <IconBrandLinkedin size={22} stroke={2} />
+              <IconBrandLinkedin size={22} stroke={2}/>
             </a>
           </ActionIcon>
           <ActionIcon size="lg" color="white" variant="subtle">
             <a href={LINKS.github} target="_blank" rel="noreferrer" style={{ color: 'white' }}>
-              <IconBrandGithubFilled size={22} stroke={2} />
+              <IconBrandGithubFilled size={22} stroke={2}/>
             </a>
           </ActionIcon>
           <ActionIcon size="lg" color="white" variant="subtle">
             <a href={LINKS.cv} target="_blank" rel="noreferrer" style={{ color: 'white' }}>
-              <IconFileCv size={22} stroke={2} />
+              <IconFileCv size={22} stroke={2}/>
             </a>
           </ActionIcon>
         </Group>
