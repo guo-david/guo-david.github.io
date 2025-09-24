a46d56b899a39559783fac0f821ce33060ffa5f2
David Guo <136103263+davidguo123456@users.noreply.github.com>
2025-09-24 10:35:39 -0400

    Update constants.ts

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
   linkedin: 'https://www.linkedin.com/in/david-guo-1994221b7/',
   github: 'https://github.com/davidguo123456',
-  cv: 'https://davidguo123456.github.io/CV-David-Guo.pdf',
+  cv: 'https://davidguo123456.github.io/cv-David-Guo-2025.pdf',
 };
